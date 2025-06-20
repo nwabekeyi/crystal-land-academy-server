e23933fd58e7098943fa208107ccee8e82cc9e8f
Nwabekeyi <chidi90simeon@gmail.com>
2025-06-20 15:59:52 +0100

    fixed form error

--- public/dist/assets/dasboardPagesContainer-CXO67BnC.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-qmmqHzY5.js";import{w as e}from"./TextField-Cs2SSotF.js";const i=s=>o=>{const{home:t}=o;return console.log("Home prop in HOC:",t),r.jsx(e,{sx:{overflow:"auto",minWidth:{sx:"90%",sm:"100%",md:"100%",lg:"100%"}},children:r.jsx(s,{...o})})};export{i as w};
